88608f88f8d3a47b935cc887b9b35220c840d444
Patricia Xiao <patricia.xiao@berkeley.edu>
2020-06-29 20:49:00 -0700

    debug

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 class CBOW(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
@@ -19,14 +20,14 @@ class SkipGram(nn.Module):
         super(SkipGram, self).__init__()
         self.u_embeddings = nn.Embedding(vocab_size, embedding_dim, sparse=True)
         self.v_embeddings = nn.Embedding(vocab_size, embedding_dim, sparse=True)
-        '''
         self.embedding_dim = embedding_dim
+        '''
         initrange = 0.5 / self.embedding_dim
         self.u_embeddings.weight.data.uniform_(-initrange, initrange)
         self.v_embeddings.weight.data.uniform_(-0, 0)
         '''
-        nn.init.normal_(self.u_embedding.weight, std=1.0 / math.sqrt(embedding_dim))
-        nn.init.normal_(self.v_embedding.weight, std=1.0 / math.sqrt(embedding_dim))
+        nn.init.normal_(self.u_embeddings.weight, std=1.0 / math.sqrt(embedding_dim))
+        nn.init.normal_(self.v_embeddings.weight, std=1.0 / math.sqrt(embedding_dim))
 
     def forward(self, u_pos, v_pos, v_neg, batch_size):
         embed_u = self.u_embeddings(u_pos)
