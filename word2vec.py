34373c2edccf502a9b64fb58711dfc38eb834bce
Patricia Xiao <patricia.xiao@berkeley.edu>
2020-07-01 01:41:35 -0700

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -51,8 +51,10 @@ class word2vec:
                 '''
                 target = np.concatenate([pos_u.copy() for _ in range(neg_v.shape[1] + 1)])
                 contex = np.concatenate([pos_v] + [neg_v[:,i] for i in range(neg_v.shape[1])])
+                labels = np.concatenate([1 for _ in pos_v] + [1 for _ in range(neg_v.shape[0] * neg_v.shape[1])])
                 print(target.shape)
                 print(contex.shape)
+                print(labels.shape)
 
                 exit(0)
 
