c6c6d340e3c9d701d928144a4f8f4ab86e694734
Patricia Xiao <patricia.xiao@berkeley.edu>
2020-07-15 03:33:18 -0700

    debug

--- word2vec.py
+++ word2vec.py
@@ -5,8 +5,13 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 from model import SkipGram, CBOW
-# from dataset import Dataset
-from dataset_old import Dataset
+
+DEBUG = False
+
+if DEBUG:
+    from dataset import Dataset
+else:
+    from dataset_old import Dataset
 
 import random
 import numpy as np
@@ -16,8 +21,10 @@ class word2vec:
     def __init__(self, input_file, model_name, vocabulary_size=100000,
                  embedding_dim=200, epoch=10, batch_size=256, windows_size=5, neg_sample_size=10):
         self.model_name = model_name
-        self.data = Dataset(input_file, vocabulary_size)
-        # self.data = Dataset(batch_size=batch_size, window_size=windows_size)
+        if DEBUG:
+            self.data = Dataset(batch_size=batch_size, window_size=windows_size)
+        else:
+            self.data = Dataset(input_file, vocabulary_size)
         self.vocabulary_size = vocabulary_size
         self.embedding_dim = embedding_dim
         self.epoch = epoch
@@ -40,50 +47,73 @@ class word2vec:
             batch_num = 0
             batch_new = 0
 
-            #for data in self.data.generate_batch(): pos_u, pos_v, neg_v = data
+            if DEBUG:
+                for data in self.data.generate_batch():
+                    pos_u, pos_v, neg_v = data
 
-            while self.data.process:
-                pos_u, pos_v, neg_v = self.data.generate_batch(self.windows_size, self.batch_size, self.neg_sample_size)
-                # pos_u, pos_v, neg_v = data
+                    '''
+                    print(pos_u.shape) # (2560,)
+                    print(pos_v.shape) # (2560,)
+                    print(neg_v.shape) # (2560, 10)
+                    exit(0)
+                    '''
 
-                '''
-                print(pos_u.shape) # (2560,)
-                print(pos_v.shape) # (2560,)
-                print(neg_v.shape) # (2560, 10)
-                exit(0)
-                '''
+                    '''
+                    target = np.concatenate([pos_u.copy() for _ in range(neg_v.shape[1] + 1)])
+                    contex = np.concatenate([pos_v] + [neg_v[:,i] for i in range(neg_v.shape[1])])
+                    labels = np.array([1 for _ in pos_v] + [-1 for _ in range(neg_v.shape[0] * neg_v.shape[1])])
+                    target = torch.LongTensor(target)
+                    contex = torch.LongTensor(contex)
+                    labels = torch.LongTensor(labels)
+                    '''
 
-                '''
-                target = np.concatenate([pos_u.copy() for _ in range(neg_v.shape[1] + 1)])
-                contex = np.concatenate([pos_v] + [neg_v[:,i] for i in range(neg_v.shape[1])])
-                labels = np.array([1 for _ in pos_v] + [-1 for _ in range(neg_v.shape[0] * neg_v.shape[1])])
-                target = torch.LongTensor(target)
-                contex = torch.LongTensor(contex)
-                labels = torch.LongTensor(labels)
-                '''
+                    pos_u = torch.LongTensor(pos_u)
+                    pos_v = torch.LongTensor(pos_v)
+                    neg_v = torch.LongTensor(neg_v)
 
-                pos_u = torch.LongTensor(pos_u)
-                pos_v = torch.LongTensor(pos_v)
-                neg_v = torch.LongTensor(neg_v)
+                    if torch.cuda.is_available():
+                        pos_u = pos_u.cuda()
+                        pos_v = pos_v.cuda()
+                        neg_v = neg_v.cuda()
 
-                if torch.cuda.is_available():
-                    pos_u = pos_u.cuda()
-                    pos_v = pos_v.cuda()
-                    neg_v = neg_v.cuda()
+                    optimizer.zero_grad()
+                    loss = model(pos_u, pos_v, neg_v, self.batch_size)
+                    # loss = model(pos_u, pos_v, neg_v, self.batch_size, target, contex, labels)
+                    loss.backward()
+                    optimizer.step()
 
-                optimizer.zero_grad()
-                loss = model(pos_u, pos_v, neg_v, self.batch_size)
-                # loss = model(pos_u, pos_v, neg_v, self.batch_size, target, contex, labels)
-                loss.backward()
-                optimizer.step()
+                    if report and batch_num % 7 == 0: # 3000
+                        end = time.time()
+                        print('epoch,batch = %2d %5d:   batch_size = %5d  loss = %4.3f\r'
+                              % (epoch, batch_num, self.batch_size, loss.item()), end="\n")
+                        batch_new = batch_num
+                        start = time.time()
+                    batch_num += 1
+            else:
+                while self.data.process:
+                    pos_u, pos_v, neg_v = self.data.generate_batch(self.windows_size, self.batch_size, self.neg_sample_size)
+                    pos_u = torch.LongTensor(pos_u)
+                    pos_v = torch.LongTensor(pos_v)
+                    neg_v = torch.LongTensor(neg_v)
 
-                if report and batch_num % 7 == 0: # 3000
-                    end = time.time()
-                    print('epoch,batch = %2d %5d:   batch_size = %5d  loss = %4.3f\r'
-                          % (epoch, batch_num, self.batch_size, loss.item()), end="\n")
-                    batch_new = batch_num
-                    start = time.time()
-                batch_num += 1
+                    if torch.cuda.is_available():
+                        pos_u = pos_u.cuda()
+                        pos_v = pos_v.cuda()
+                        neg_v = neg_v.cuda()
+
+                    optimizer.zero_grad()
+                    loss = model(pos_u, pos_v, neg_v, self.batch_size)
+                    # loss = model(pos_u, pos_v, neg_v, self.batch_size, target, contex, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                    if report and batch_num % 7 == 0: # 3000
+                        end = time.time()
+                        print('epoch,batch = %2d %5d:   batch_size = %5d  loss = %4.3f\r'
+                              % (epoch, batch_num, self.batch_size, loss.item()), end="\n")
+                        batch_new = batch_num
+                        start = time.time()
+                    batch_num += 1
 
         model.save_embeddings(self.data.idx2word, 'word_embdding.txt', torch.cuda.is_available())
 
